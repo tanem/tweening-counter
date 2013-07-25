7bef019a5378927536d3bb42af9dcfe3fcffa8bb
tanem <tane.morgan@gmail.com>
2013-07-26 08:35:43 +1200

    Updated Gruntfile with new tasks. Added Karma in place of Jasmine contrib.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,83 @@
+'use strict';
+
 module.exports = function(grunt){
 
   grunt.initConfig({
+
+    meta: {
+      src: 'src/*.js',
+      test: 'test/*.js'
+    },
+
+    clean: {
+      coverage: {
+        src: '_coverage'
+      },
+      dist: {
+        src: '_dist'
+      }
+    },
 
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
-      src: 'src/counter.js',
-      test: 'test/counter.js'
+      src: '<%= meta.src %>',
+      test: '<%= meta.test %>'
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          '_dist/counter.min.js': '<%= meta.src %>'
+        }
+      }
     },
 
     watch: {
-      files: ['<%= jshint.src %>', '<%= jshint.test %>'],
-      tasks: 'test'
+      files: ['<%= meta.src %>', '<%= meta.test %>'],
+      tasks: ['jshint', 'test']
     },
 
-    jasmine: {
-      pivotal: {
-        src: '<%= jshint.src %>',
-        options: {
-          specs: '<%= jshint.test %>',
-          helpers: 'test/helper.js',
-          keepRunner: true,
-          outfile: 'SpecRunner.html'
+    karma: {
+      options: {
+        configFile: 'karma.conf.js',
+        runnerPort: 9100,
+        background: false,
+        singleRun: true
+      },
+      headless: {
+        browsers: ['PhantomJS'],
+        reporters: ['dots']
+      },
+      test: {
+        browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+        reporters: ['dots']
+      },
+      cover: {
+        browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+        reporters: ['dots', 'coverage'],
+        coverageReporter: {
+          type: 'html',
+          dir: '_coverage'
+        },
+        preprocessors: {
+          'src/*.js': 'coverage'
         }
       }
     }
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('test', ['karma:test']);
+  grunt.registerTask('cover', ['clean:coverage', 'karma:cover']);
   grunt.registerTask('start', ['test', 'watch']);
+  grunt.registerTask('dist', ['clean:dist', 'jshint', 'test', 'uglify:dist']);
 
 };
